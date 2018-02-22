dc818dbda5dfd457602056de2434a5d7f9dcfc08
Miroslav Parafeniuk <mrs-mouse@mail.ru>
2018-02-22 17:21:00 +0300

    Added Gauss() function for solving linear equations system.

--- ProjectEuler/main.cpp
+++ ProjectEuler/main.cpp
@@ -706,6 +706,48 @@ vector<string> parse_by_symbol (const string &S, char p)
     return v;
 }
 
+int Gauss (vector <vector<dd>> a, vector<dd> & ans) // 0 - no solutions, 1 - one solution, 2 - infinitely many solutions
+{
+    int n = (int) a.size();
+    int m = (int) a[0].size() - 1;
+    
+    vector<int> where (m, -1);
+    for (int col=0, row=0; col<m && row<n; col++) {
+        
+        int sel = row;
+        for (int i=row; i<n; i++) {
+            if (abs (a[i][col]) > abs (a[sel][col])) sel = i;
+        }
+        if (abs (a[sel][col]) < EPS) continue;
+        for (int i=col; i<=m; i++) swap (a[sel][i], a[row][i]);
+        where[col] = row;
+        
+        for (int i=0; i<n; i++) {
+            if (i != row) {
+                dd c = a[i][col] / a[row][col];
+                for (int j=col; j<=m; ++j) a[i][j] -= a[row][j] * c;
+            }
+        }
+        row++;
+    }
+    
+    ans.assign (m, 0);
+    for (int i=0; i<m; i++) {
+        if (where[i] != -1) ans[i] = a[where[i]][m] / a[where[i]][i];
+    }
+    for (int i=0; i<n; i++) {
+        dd sum = 0;
+        for (int j=0; j<m; ++j) sum += ans[j] * a[i][j];
+        if (abs (sum - a[i][m]) > EPS) return 0;
+    }
+    
+    for (int i=0; i<m; ++i) {
+        if (where[i] == -1) return 2;
+    }
+    
+    return 1;
+}
+
 vector<ull> Lagged_Fibonacci_Generator (int n)
 {
     vector<ull> s;
