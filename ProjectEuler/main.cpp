826eb70c809d24c6e7f11e64f8cf32d86e1a710f
Miraslau Parafeniuk <mrs-mouse@mail.ru>
2021-09-02 22:49:48 +0300

    Optimization and refactoring

--- ProjectEuler/main.cpp
+++ ProjectEuler/main.cpp
@@ -22,20 +22,14 @@ using namespace Containers;
 #define N 14
 #define TUBE_SIZE 4
 #define EMPTY_TUBES 2
-#define tube vchar
 #define pic pair<int,char>
 
-tube A[N];
-void fill_tube (int k, const string &S)
-{
-    A[k].clear();
-    for (int i=0; i<(int)S.length()/*=4*/; i++) A[k].push_back(S[i]);
-}
+string A[N];
 
 // TODO: can be calculated dynamically
 int depth (int k) // returns how many identical blocks of water from the top have the same type
 {
-    if (A[k].empty()) return 0; // possibly will be called for non-empty tubes only, so could be removed
+    //if (A[k].empty()) return 0; // it is never called for empty tubes only, so could be removed
     int cnt = 1, id = (int)A[k].size()-2;
     while (id >= 0 && A[k][id] == A[k].back()) { cnt++; id--; }
     return cnt;
@@ -48,7 +42,7 @@ bool one_color (int k) // checks if tube A[k] consists of only one water type
 
 int pouring_count (int from, int to) // returns how many cells we can pour, if it makes any sense
 {
-    if (A[from].empty()) return 0; // nothing to pour
+    //if (A[from].empty()) return 0; // nothing to pour (never called for empty, same as for depth)
     if (A[to].size() == TUBE_SIZE) return 0; // nowhere to pour
     
     char cf = A[from].back(), ct = '#'; // color from, color to (is unknown yet)
@@ -67,46 +61,51 @@ int pouring_count (int from, int to) // returns how many cells we can pour, if i
     if (d == 2) { // still one complex move can make sense
         for (int i=0; i<N; i++) {
             if (i == from || i == to) continue;
-            if (A[i].size() != TUBE_SIZE && A[i].back() == cf) return 5;
+            if (A[i].size() != TUBE_SIZE && A[i].back() == cf) return (i+1)*10;
         }
     }
     
     return 0; // otherwise move makes no sense
 }
 
-vector<pair<pii,pic>> moves; // < <from, to>, <how many, water type> >
+vector<pair<pii,int>> moves; // < <from, to>, how many >
 int one_color_count = 0, empty_count = EMPTY_TUBES;
 vector<bool> OC(N); // one color tubes storage
 bool solution_found = false;
 
+void move_back()
+{
+    int from = moves.back().fs.fs, to = moves.back().fs.sc, wt = A[to].back();
+    for (int i=0; i<(int)moves.back().sc; i++) {
+        A[from].push_back(wt);
+        A[to].pop_back();
+    }
+    moves.pop_back();
+}
+
 void dfs (int from, int to) // initial pouring is guaranteed to be valid
 {
-    if (solution_found) return;
-    
+    // check if pouring is valid
     int cnt = pouring_count(from,to);
     if (cnt == 0) return; // failed move
     char wt = A[from].back(); // water type
     int to2 = -1;
     
     // perform pouring
-    if (cnt == 5) {
-        for (int i=0; i<N; i++) { // set additional tube we pour to
-            if (i == from || i == to) continue;
-            if (A[i].size() != TUBE_SIZE && A[i].back() == wt) { to2 = i; break; }
-        }
-        A[from].pop_back();
-        A[from].pop_back();
+    if (cnt > 4) {
+        to2 = cnt/10 - 1; // decode it
+        A[from].resize(A[from].size()-2);
         A[to].push_back(wt);
         A[to2].push_back(wt);
-        moves.push_back(mp(mp(from,to),mp(1,wt)));
-        moves.push_back(mp(mp(from,to2),mp(1,wt)));
+        moves.push_back(mp(mp(from,to),1));
+        moves.push_back(mp(mp(from,to2),1));
     }
     else {
         for (int i=0; i<cnt; i++) { A[from].pop_back(); A[to].push_back(wt); }
-        moves.push_back(mp(mp(from,to),mp(cnt,wt)));
+        moves.push_back(mp(mp(from,to),cnt));
     }
     
-    // update_values
+    // update values
     int oc_change = 0, emp_change = 0;
     bool from_reverse = false, to_reverse = false;
     if (OC[from]) { // one colored tube becomes empty
@@ -115,7 +114,7 @@ void dfs (int from, int to) // initial pouring is guaranteed to be valid
     else { // multi colored tube becomes one colored
         if (one_color(from)) { OC[from] = true; from_reverse = true; oc_change++; }
     }
-    // empty tube becomes one colored (case cnt == 5 is impossible here)
+    // empty tube becomes one colored (case cnt > 4 is impossible here)
     if (A[to].size() == cnt) { OC[to] = true; to_reverse = true; oc_change++; emp_change--; }
     one_color_count += oc_change;
     empty_count += emp_change;
@@ -124,29 +123,21 @@ void dfs (int from, int to) // initial pouring is guaranteed to be valid
     if (one_color_count == N-EMPTY_TUBES && empty_count == EMPTY_TUBES) { solution_found = true; return; }
     
     // go deep
-    for (int i=0; i<N; i++) for (int j=0; j<N; j++) {
-        if (i != j) dfs(i,j);
+    for (int i=0; i<N; i++) {
+        if (A[i].empty()) continue;
+        for (int j=0; j<N; j++) if (i != j) {
+            dfs(i,j);
+            if (solution_found) return;
+        }
     }
-    
-    if (solution_found) return;
     
     // reverse move back
     if (from_reverse) OC[from] = !OC[from];
     if (to_reverse)   OC[to]   = !OC[to];
     one_color_count -= oc_change;
-    empty_count -= emp_change;
-    for (int i=0; i<(int)moves.back().sc.fs; i++) {
-        A[from].push_back(wt);
-        A[to].pop_back();
-    }
-    moves.pop_back();
-    if (cnt == 5) { // do it one more time
-        for (int i=0; i<(int)moves.back().sc.fs; i++) {
-            A[from].push_back(wt);
-            A[to].pop_back();
-        }
-        moves.pop_back();
-    }
+    empty_count    -= emp_change;
+    move_back();
+    if (cnt > 4) move_back(); // one more time in this case
 }
 
 int main() {
@@ -176,15 +167,12 @@ int main() {
      g: grey
      */
     
-    for (int i=0; i<N-EMPTY_TUBES; i++) {
-        string S; cin >> S;
-        fill_tube(i,S);
-    }
+    for (int i=0; i<N-EMPTY_TUBES; i++) cin >> A[i];
     
     for (int k=0; k<N-EMPTY_TUBES; k++) {
         dfs(k,N-EMPTY_TUBES);
         if (solution_found) {
-            cout << "SOLUTION:\n";
+            cout << "SOLUTION IN " << moves.size() << " MOVES:\n";
             for (int i=0; i<(int)moves.size(); i++) cout << moves[i].fs.fs+1 << " -> " << moves[i].fs.sc+1 << endl;
             break;
         }
