58bb4d1a763bf592c4f99a1db7a5805a8fe46d10
Miraslau Parafeniuk <miraslau.parafeniuk@coople.com>
2023-04-10 16:51:50 +0200

    First approach

--- ProjectEuler/main.cpp
+++ ProjectEuler/main.cpp
@@ -31,6 +31,50 @@ int main() {
     
     ull ans = 0;
     
+    const int N = 1000000;
+    Eratosthenes_sieve(N, true);
+    
+    dd res = 0;
+    vbool used(N);
+    
+    for (ull n=3; n<=N; n+=10) {
+        vpull F = factorize(n);
+        if (F.size() == 1) used[F[0].fs] = true;
+    }
+    
+    for (ull n=3; n<=N; n+=10) {
+        vpull F = factorize(n);
+        bool already = false;
+        for (int i=0; i<(int)F.size(); i++) {
+            if (used[F[i].fs]) { already = true; break; }
+        }
+        if (!already && F.size() == 2) {
+            cout << n << ": ";
+            for (int i=0; i<(int)F.size(); i++) cout << F[i].fs << " "; cout << endl;
+        }
+    }
+    
+    vull D;
+    for (ull n=3; n<=N; n+=10) {
+        bool coprime = true;
+        for (int i=0; i<(int)D.size(); i++) {
+            if (n % D[i] == 0) { coprime = false; break; }
+        }
+        if (coprime) {
+            vpull F = factorize(n);
+            D.push_back(F[0].fs);
+            res += log((dd)F[0].fs) / log(2.718281828459);
+        }
+        vpull F = factorize(n);
+        cout << n << ": ";
+        for (int i=0; i<(int)F.size(); i++) cout << F[i].fs << " "; cout << endl;
+    }
+    
+    res -= log(37.0) / log(2.718281828459);
+    res -= log(47.0) / log(2.718281828459);
+    res += log(59.0) / log(2.718281828459);
+    cout << fixed << res;
+    
     cout << endl << ans << endl;
     Total_Time = clock() - Total_Time;
     cout << "Running time: " << ((float)Total_Time)/CLOCKS_PER_SEC << " seconds\n";
