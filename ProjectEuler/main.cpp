772a1c25fe52b5f700078bd0c1aa46fcb9d60b80
parfenuk <pp.miroslav@gmail.com>
2024-01-04 13:39:54 +0100

    For 4 topics

--- ProjectEuler/main.cpp
+++ ProjectEuler/main.cpp
@@ -41,41 +41,43 @@ int main() {
     }
     
     const vector<string> w = {
-        "Covers","Trio","Verse",          //  0  1  2
-        "Nick","Mike","Sings",            //  3  4  5
-        "1Letter","Plays","Eating",       //  6  7  8
-        "Drinking","TV","Duets",          //  9 10 11
-        "SS","EE","OO",                   // 12 13 14
-        "Instrumentals","Mash-ups","Game" // 15 16 17
+        "Abbreviatures","AI","Animals",   //  0  1  2
+        "Classic","Female","FinnishRock", //  3  4  5
+        "FinnishCovers","90s","Unfamous", //  6  7  8
+        "Games","Guitarists","Left",      //  9 10 11
+        "MAC","Minuses","SingEng",        // 12 13 14
+        "SingRus","Name","NoEnglish",     // 15 16 17
+        "OI","Options","Pop",             // 18 19 20
+        "RadioTapok","Riffs","The5"       // 21 22 23
     };
-    const vector<int> cats = { 3,7,8,15,16 };
-    const vector<int> auctions = { 2,4,10,11,12,13,17 };
+    const vector<int> cats = { 5,6,10,13,14 };
+    const vector<int> auctions = { 1,2,15,18,19,22,23 };
     const vector<int> cats_distribution = { 2,1,2 }; // cats in each round
-    const vector<int> auctions_distribution = { 2,1,2 }; // same for auctions
+    const vector<int> auctions_distribution = { 1,1,1,1 }; // same for auctions
     
-    vpii F = {mp(12,13),mp(12,14),mp(13,14),mp(10,17)}; // forbidden pairs, should be in different trios
-    vpii R = {mp(8,9),mp(3,4)}; // required pairs, should be together
-    vpii C = {mp(11,1),mp(12,1),mp(15,1),mp(1,2),mp(2,3),mp(6,3)}; // some elements must be in specific trio
-    vpii D = {mp(0,1),mp(1,1),mp(3,1),mp(8,1),mp(10,1),mp(16,1)}; // some elements shouldn't be in specific trio
+    vpii F = {mp(12,18)}; // forbidden pairs, should be in different trios
+    vpii R = {mp(9,23),mp(14,15)}; // required pairs, should be together
+    vpii C = {mp(6,1),mp(11,1),mp(13,2),mp(16,2),mp(5,2),mp(8,2)}; // some elements must be in specific trio
+    vpii D = {mp(23,1),mp(23,2),mp(22,3),mp(22,4),mp(14,3),mp(14,4),mp(1,3),mp(1,4),mp(2,1),mp(2,2),mp(10,1),mp(5,3),mp(5,4),mp(4,3),mp(4,4),mp(3,1),mp(21,4)}; // some elements shouldn't be in specific trio
     
-    vint v; for (int i=0; i<18; i++) v.push_back(i);
+    vint v; for (int i=0; i<(int)w.size(); i++) v.push_back(i);
     random_device rd;
     mt19937 g(rd());
     
-    vint A(18);
+    vint A(w.size());
     bool ok = false;
     
     while (!ok) {
         ans++;
         shuffle(v.begin(), v.end(), g);
-        vector<int> catscnt(3), auccnt(3);
-        for (int i=0; i<18; i++) {
+        vector<int> catscnt(w.size()/6), auccnt(w.size()/6);
+        for (int i=0; i<(int)w.size(); i++) {
             A[v[i]] = i/6;
-            if (contains(cats,v[i])) catscnt[i/6]++;
+            //if (contains(cats,v[i])) catscnt[i/6]++;
             if (contains(auctions,v[i])) auccnt[i/6]++;
         }
-        if (catscnt[0] != cats_distribution[0] ||
-            catscnt[1] != cats_distribution[1]) continue;
+//        if (catscnt[0] != cats_distribution[0] ||
+//            catscnt[1] != cats_distribution[1]) continue;
         if (auccnt[0] < auctions_distribution[0] ||
             auccnt[1] < auctions_distribution[1] ||
             auccnt[2] < auctions_distribution[2]) continue;
@@ -101,10 +103,11 @@ int main() {
     
     sort(v.begin(), v.begin()+6);
     sort(v.begin()+6, v.begin()+12);
-    sort(v.begin()+12, v.end());
+    sort(v.begin()+12, v.begin()+18);
+    sort(v.begin()+18, v.end());
     
     cout << "FOUND after " << ans << " iterations\n";
-    for (int i=0; i<18; i++) {
+    for (int i=0; i<(int)w.size(); i++) {
         if (i%6 == 0) cout << endl;
         cout << w[v[i]] << " ";
     }
