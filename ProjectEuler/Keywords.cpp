dbe6890142ec7ea5808346929e19f560f4ff9f11
parfenuk <pp.miroslav@gmail.com>
2024-06-09 03:04:28 +0200

    Added C++ keywords list

---
+++ ProjectEuler/Keywords.cpp
@@ -0,0 +1,85 @@
+//
+//  Keywords.cpp
+//  ProjectEuler
+//
+//  Created by Miraslau Parafeniuk on 9.06.24.
+//  Copyright © 2024 Miraslau Parafeniuk. All rights reserved.
+//
+
+alignof // C++11
+asm
+auto
+bool
+break
+case
+catch
+char
+char16_t // C++11
+char32_t // C++11
+class
+concept // C++20
+const
+const_cast
+consteval // C++20
+constexpr // C++11
+constinit // C++20
+continue
+decltype // C++11
+default
+delete
+do
+double
+dynamic_cast
+else
+enum
+explicit
+export
+extern
+false
+float
+for
+friend
+goto
+if
+inline
+int
+long
+module // C++20
+mutable
+namespace
+new
+noexcept // C++11
+nullptr // C++11
+operator
+private
+protected
+public
+register
+reinterpret_cast
+requires // C++20
+return
+short
+signed
+sizeof
+static
+static_assert // C++11
+static_cast
+struct
+switch
+template
+this
+thread_local // C++11
+throw
+true
+try
+typedef
+typeid
+typename
+union
+unsigned
+using
+virtual
+void
+volatile
+wchar_t
+while
