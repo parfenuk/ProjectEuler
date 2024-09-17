b78eea3c0335479196e0f314977cf18385acd5ee
parfenuk <pp.miroslav@gmail.com>
2024-09-17 04:54:02 +0200

    Average Least Common Multiple.

--- ProjectEuler/main.cpp
+++ ProjectEuler/main.cpp
@@ -19,15 +19,80 @@
 using namespace Algebra;
 using namespace Containers;
 
-const vull F = Combinatorics::generate_factorials(12);
+const int Q = 999999017;
+const ll i2 = inverse(2,Q);
+const ll i6 = inverse(6,Q);
 
-ull process_vector (vsint a, bool is_final = false)
+const int M = 7000000; // cache size
+vll r(M+1,-1);
+map<ll,ll> rc;
+
+void set_caches()
 {
-    ull k = F[12-is_final];
-    for (int i=0; i<(int)a.size(); i++) k /= F[a[i]];
-    if (is_final || a[0] == 0) return k;
-    a[0]--;
-    return k - process_vector(a,true);
+    EulerPhiSieve(M);
+    r[1] = 1;
+    for (ll i=2; i<=M; i++) {
+        r[i] = (r[i-1] + i*EulerPhi(i)) % Q;
+    }
+}
+
+ll S1 (ll n) { n %= Q; return n*(n+1) % Q * i2 % Q; }
+ll S2 (ll n) { n %= Q; return n*(n+1) % Q * (2*n+1) % Q * i6 % Q; }
+
+ll R (ll n) // calculates Sum[k*phi(k), 1 <= k <= n]
+{
+    if (n <= M && r[n] != -1) return r[n];
+    if (n > M && rc.find(n) != rc.end()) return rc[n];
+    
+    ll res = 0;
+    ll q = NumberUtils::lower_integer_sqrt(n);
+    
+    // using this formula: https://math.stackexchange.com/questions/316376/how-to-calculate-these-totient-summation-sums-efficiently
+    for (ll k=2; k<=n/q; k++) {
+        res += (Q + S1(k) - S1(k-1)) * R(n/k);
+        res %= Q;
+    }
+    for (ll m=1; m<q; m++) {
+        res += (Q + S1(n/m) - S1(n/(m+1))) * R(m);
+        res %= Q;
+    }
+    res = (Q + S2(n) - res) % Q;
+    
+    if (n <= M) r[n] = res;
+    else rc[n] = res;
+    return res;
+}
+
+ll R (ll a, ll b)
+{
+    ll res = R(b) - R(a-1);
+    if (res < 0) res += Q;
+    return res;
+}
+
+ull F (ll N)
+{
+    ull ans = N % Q;
+    
+    const ll n = NumberUtils::lower_integer_sqrt(N);
+    
+    ull sum = 0;
+    for (ll k=2; k<=n; k++) {
+        sum += (N/k) % Q * k % Q * EulerPhi(k) % Q;
+        sum %= Q;
+    }
+    for (ll a=1; a<=n; a++) {
+        ll k1 = N/(a+1) + 1, k2 = N/a;
+        if (k1 <= n) k1 = n+1;
+        if (k1 > k2) continue;
+        
+        sum += a * R(k1,k2) % Q;
+        sum %= Q;
+    }
+    
+    ans = (ans + sum*i2) % Q;
+    
+    return ans;
 }
 
 int main() {
@@ -42,11 +107,9 @@ int main() {
     
     ull ans = 0;
     
-    vvsint a = Combinatorics::get_repeated_combinations(10,12);
-    for (int i=0; i<(int)a.size(); i++) {
-        ull k = process_vector(a[i]);
-        ans += k*(k-1)/2;
-    }
+    set_caches();
+    
+    ans = F(99999999019);
     
     cout << endl << ans << endl;
     Total_Time = clock() - Total_Time;
