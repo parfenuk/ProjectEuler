4b6d73dd21d74ae8d86efa1209986b97a0570c19
parfenuk <pp.miroslav@gmail.com>
2026-04-11 15:07:49 +0200

    Fruit Salad.

--- ProjectEuler/main.cpp
+++ ProjectEuler/main.cpp
@@ -32,6 +32,27 @@ int main() {
     
     ull ans = 0;
     
+    const int N = 10000000;
+    
+    Eratosthenes_sieve(N);
+    // works much faster with 'Eratosthenes_sieve' call before
+    vector<char> mu = MoebuisMuSieve(N);
+    
+    for (ll r=1; r*r<=N; r++) {
+        for (ll s = NumberUtils::lower_integer_sqrt(3*r*r)+1; s*s < 4*r*r; s++) {
+            ll x = 4*r*r - s*s;
+            ll z = s*s - 3*r*r;
+            ll t1 = r*(2*r+s), t2 = r*(2*r-s);
+            for (ll d=1;;d++) {
+                if (mu[d] == 0) continue;
+                if (t2 > z && (x+t2)*d > N) break;
+                if (t2 <= z && (x+t1)*d > N) break;
+                if ((x+t1)*d <= N) ans += (x+t1)*d;
+                if (t2 > z) ans += (x+t2)*d;
+            }
+        }
+    }
+    
     cout << endl << ans << endl;
     Total_Time = clock() - Total_Time;
     cout << "Running time: " << ((float)Total_Time)/CLOCKS_PER_SEC << " seconds\n";
