ad0e7d229fb26409eb50f950e69e9b64cdd0c19d
parfenuk <pp.miroslav@gmail.com>
2026-06-08 13:32:26 +0200

    Long Products.

--- ProjectEuler/main.cpp
+++ ProjectEuler/main.cpp
@@ -20,6 +20,52 @@
 using namespace Algebra;
 using namespace Containers;
 
+const int Q = 1234567891;
+
+vull inv;
+map<ll,vll> M[30];
+
+ll C (ll N, int k)
+{
+    ll s = 1;
+    for (ll n=N; n>N-k; n--) s = s*n % Q;
+    for (ll i=2; i<=k; i++) s = s*inv[i] % Q;
+    return s;
+}
+
+ll F (ll N, int m, int k) // k^m <= N is always checked before
+{
+    if (m == 1) return 1;
+    if (m == 2) return 1 + 2*max(0ll,N/k-k);
+    vll &v = M[m][N];
+    if (v.size() > k && v[k] != -1) return v[k];
+    
+    ll sum = 1;
+    
+    ll k1p = 1;
+    vll kp = Combinatorics::generate_powers((ll)k,m-1,Q);
+    for (int p=m-1; p>=1; p--) {
+        k1p = k1p*(k+1)%Q; // powmod(k+1,m-p)
+        ll nkp = N/kp[p];
+        if (k1p > nkp) break;
+        
+        ll s = 0;
+        for (int i=k+1;;i++) {
+            if (powmod(i,m-p) > nkp) break;
+            ll val = F(nkp,m-p,i);
+            s += val;
+            if (s >= Q) s -= Q;
+        }
+        s = s*C(m,p) % Q;
+        sum += s;
+        if (sum >= Q) sum -= Q;
+    }
+    
+    while (v.size() <= k) v.push_back(-1);
+    v[k] = sum;
+    return sum;
+}
+
 int main() {
     clock_t Total_Time = clock();
     cout.precision(12);
@@ -31,6 +77,24 @@ int main() {
 #endif
     
     ull ans = 0;
+    
+    const ll N = 1000000000;
+    
+    inv = Algebra::generate_inverses(30,Q);
+    ans = 1 + N*(N-1)%Q; // case 0 and 1 values > 2
+    
+    for (int m=2;;m++) {
+        cout << "m = " << m << endl;
+        if (powmod(2,m) > N) break;
+        ll s = 0;
+        for (int k=2; powmod(k,m)<=N; k++) {
+            s += F(N,m,k);
+            if (s >= Q) s -= Q;
+        }
+        s = s*C(N,m)%Q;
+        ans += s;
+        if (ans >= Q) ans -= Q;
+    }
     
     cout << endl << ans << endl;
     Total_Time = clock() - Total_Time;
