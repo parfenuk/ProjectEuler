9a76247552f222871167bb2a85ff8711f2d534cb
parfenuk <pp.miroslav@gmail.com>
2026-06-08 12:42:24 +0200

    Polar Polygons.

--- ProjectEuler/main.cpp
+++ ProjectEuler/main.cpp
@@ -32,6 +32,43 @@ int main() {
     
     ull ans = 0;
     
+    const ll Q = 1000000007;
+    const ll N = powmod(7,13);
+    EulerPhiSieve(7000000,Q);
+    
+    vector<pair<ll,pll>> A = get_division_blocks(N);
+    vector<pair<ll,pll>> B(A.size()); // <value, <count % Q, count % (Q-1)>> // last needed for correct power
+    for (int i=0; i<(int)A.size(); i++) {
+        ll v = A[i].fs % Q;
+        ll c = EulerPhiSum(A[i].sc.sc,Q) - EulerPhiSum(A[i].sc.fs-1,Q);
+        c = (c+Q)*4 % Q;
+        B[i].fs = v;
+        B[i].sc.fs = c;
+    }
+    
+    for (int i=1; i<=7000000; i++) {
+        eulerPhiSum[i] = eulerPhiSum[i-1] + eulerPhi[i];
+        if (eulerPhiSum[i] >= Q-1) eulerPhiSum[i] -= Q-1;
+    }
+    eulerCache.clear();
+    for (int i=0; i<(int)A.size(); i++) {
+        ll c = EulerPhiSum(A[i].sc.sc,Q-1) - EulerPhiSum(A[i].sc.fs-1,Q-1);
+        c = (c+Q-1)*4 % (Q-1);
+        B[i].sc.sc = c;
+    }
+    
+    ll t = 1;
+    for (int i=0; i<(int)B.size(); i++) {
+        t = t*powmod(B[i].fs+1,B[i].sc.sc,Q) % Q;
+    }
+    ll n = N%Q;
+    ll b = n*(n+1)%Q*4%Q * t%Q;
+    for (int i=0; i<(int)B.size(); i++) {
+        b -= B[i].fs*B[i].fs%Q * B[i].sc.fs%Q;
+        if (b < 0) b += Q;
+    }
+    ans = (t*t + Q - b - 1) % Q;
+    
     cout << endl << ans << endl;
     Total_Time = clock() - Total_Time;
     cout << "Running time: " << ((float)Total_Time)/CLOCKS_PER_SEC << " seconds\n";
